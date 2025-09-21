d9f66e0aaef6d8c755992b2201eb493c4c7d2ca4
JawanJayy <jawanjackson2023@gmail.com>
2025-09-21 11:45:05 -0400

    Add files via upload

---
+++ python P1LAB1_JacksonJawan.py
@@ -0,0 +1,12 @@
+# Jawan Jackson
+# 09/21/2025
+# P1LAB1 – Introduction to Python
+# This program asks the user for their first and last names
+# and displays a welcome message including their full name.
+
+# Get user input
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Display output
+print("Hello,", first_name, last_name + "! Welcome to CTI-110")
